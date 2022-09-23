cb8c3b148cabd35949201f8ee7ea75941bb11321
Idad Wind <89760671+WillamSun@users.noreply.github.com>
2022-09-23 09:01:52 +0800

    Update main.js

--- KCISbaGua/main.js
+++ KCISbaGua/main.js
@@ -16,7 +16,7 @@ while(sword!="527647021")
 {
   sword=prompt("请输入密码登录");
   if (sword==null)
-  {window.location.("about:blank");}
+  {location.href("about:blank");}
   sword=getHashCode(sword);
   if (sword!="527647021")
   {alert("密码错误");}
