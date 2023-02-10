a9fd396483a8abac693443f4d7d15b25d85c1a51
Idad Wind <89760671+WillamSun@users.noreply.github.com>
2023-02-10 15:29:53 +0800

    Update

--- Game/Scripts.js
+++ Game/Scripts.js
@@ -1,7 +1,10 @@
-sum = 0;
-selected = 0;
-label = 7;
-score = 0;
+var sum = 0;
+var selected = 0;
+var label = 7;
+var score = 0;
+var gameover = false;
+var gameover_text = "";
+var numbers_l = [];
 function init_label(){
 	i = (Math.random()*10-10).toFixed(0);
 	if (i<0) return "(" + i.toString() + ")";
@@ -11,19 +14,31 @@ function init_label(){
 function init_labels(numbers = 7){
 	sum = 0;
 	selected = 0;
-}
-function reset(){
-	init_labels(label);
-}
-function submit(){
-	alert(init_label().toString());
-	if (sum == 0 && selected != 0){
-		
+	for (var i = 0; i < numbers; i++){
+		numbers_l.push(init_label())
 	}
 }
-function switch_color_scheme(){
-	if ($("body").hasClass("night"))
+$(document).ready(function(){
+  $("#SwitchColorScheme").click(function(){
+	if ($("body").hasClass("night")){
 		$("body").removeClass("night");
-	else
+		$("#SwitchColorScheme").val("Dark Mode");
+	}
+	else{
 		$("body").addClass("night");
-}
+		$("#SwitchColorScheme").val("Light Mode");
+	}
+  });
+  $("#Submit").click(function(){
+	if (sum == 0 && selected != 0){
+		score++;
+	}
+	else if (selected != 0){
+		gameover = true;
+		if (sum==0) gameover_text = "equals" + sum + " and it's not equals to 0"
+	}
+  });
+  $("#Reset").click(function(){
+	init_labels(label);
+  });
+});
