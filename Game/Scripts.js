23be016a1937f3f4264b1bb1e6b27cb545b0ec78
Idad Wind <89760671+WillamSun@users.noreply.github.com>
2023-02-15 06:47:22 +0800

    Update

--- Game/Scripts.js
+++ Game/Scripts.js
@@ -6,19 +6,38 @@ var gameover = false;
 var gameover_text = "";
 var numbers_l = [];
 function init_label(){
-	i = (Math.random()*10-10).toFixed(0);
+	i = parseInt(Math.random()*19-9,10);
 	if (i<0) return "(" + i.toString() + ")";
 	if (i==0) return "(+0)";
 	return "(+" + i.toString() + ")";
 }
+function numberbuttons_clicked(){
+	if ($(this).hasClass("submit_button")){
+		$(this).removeClass("submit_button");
+		$(this).addClass("button");
+		selected--;
+		sum -= numbers_l[parseInt($(this).attr('id'))];
+	}
+	else {
+		$(this).addClass("submit_button");
+		$(this).removeClass("button");
+		selected++;
+		sum += numbers_l[parseInt($(this).attr('id'))];
+	}
+}
 function init_labels(numbers = 7){
 	sum = 0;
 	selected = 0;
+	$("#numbers").empty();
 	for (var i = 0; i < numbers; i++){
-		numbers_l.push(init_label())
+		n = init_label();
+		numbers_l.push(n)
+		$("#numbers").append("<input type=\"button\" id=\"" + (i+1).toString() + "\" value=\"" + n + "\" class=\"buttons button numberbuttons\">");
 	}
+	$(".numberbuttons").bind("click", numberbuttons_clicked);
 }
 $(document).ready(function(){
+	init_labels();
 	$("#SwitchColorScheme").click(function(){
 		if ($("body").hasClass("night")){
 			$("body").removeClass("night");
