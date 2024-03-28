1a60125b0a79b39a54b4492f609c9e6d1fa7d421
Idad Wind <89760671+WillamSun@users.noreply.github.com>
2024-03-28 21:23:01 +0800

    Update Info

--- grades/source/view.js
+++ grades/source/view.js
@@ -35,11 +35,18 @@ function loadData(){
 		(stud.grade == grade_option || grade_option == 'a')){
 			console.log(stud);
 			html += '<li class="table-row">' +
-			`<div class="col col-1" data-label="Id">${stud.id}</div>` +
-			`<div class="col col-2" data-label="Name">${stud.name}</div>` +
-			`<div class="col col-2" data-label="Pinyin">${stud.pinyin}</div>` +
-			`<div class="col col-1" data-label="Birthday">${stud.birthday}</div>` +
-			'</li>';
+			`<div class="col col-1" data-label="Id">${stud.id}</div>
+			<div class="col col-2" data-label="Name">${stud.name}</div>
+			<div class="col col-2" data-label="Pinyin">${stud.pinyin}</div>
+			<div class="col col-1" data-label="Birthday">${stud.birthday}</div>
+			<div class="col col-2" data-label="Homeroom">${stud.homeroom}</div>
+			<div class="col col-2" data-label="Card Id">${stud.card_id}</div>
+			<div class="col col-2" data-label="Dorm">
+				<input type="checkbox" ${stud.isBoarded?'checked':''} disabled>Boarded
+				<br>
+				<input type="checkbox" ${stud.doStayAtSelfStudy?'checked':''} disabled>Self Study
+		  	</div>
+			</li>`;
 		}
 	}
 	table.innerHTML = html;
